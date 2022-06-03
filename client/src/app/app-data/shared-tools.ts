48c714fbb2eac4427d681c9f94bca8d648320b73
Rui Wang <ruwang@hanover.com>
2022-06-03 13:55:13 -0400

    added a list of tools to front page

---
+++ client/src/app/app-data/shared-tools.ts
@@ -0,0 +1,19 @@
+export class SharedTools {
+	constructor(
+		public name: string,
+		public location: string,
+		public description: string
+	) {	}
+}
+
+export const sharedTools = [
+	new SharedTools('API Client', 'server/docs/apicalls.http', 'use Visual Studio Code API Client extension for quick API tests'),
+	new SharedTools('form-controls', 'client/src/app/modules/shared/form-controls', 'wrapper classes for date, input, \
+	multi-choices, single-choice, radio button group'),
+	new SharedTools('default value pipe', 'client/src/app/modules/shared/default-value.pipe.ts', 'populate default value N/A for a \
+	null-valued form control'),
+	new SharedTools('snack-bar', 'client/src/app/modules/shared/misce/snack-bar', 'pop-up bar at the bottom to display notification \
+	for a few seconds'),
+	new SharedTools('spinner', 'client/src/app/modules/shared/misce/spinner', 'a spinner displayed when app is retrieving data'),
+	new SharedTools('tree', 'client/src/app/modules/shared/misce/tree', 'a visual tree for data structure documentation')
+];
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { sharedTools } from 'src/app/app-data/shared-tools';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  sharedTools = sharedTools;
+  
   constructor() { }
 
   ngOnInit(): void {
