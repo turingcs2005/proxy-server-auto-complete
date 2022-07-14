6e1f2f89745c30205ecc66d8cdff47c608c8be79
Rui Wang <ruwang@hanover.com>
2022-07-14 13:25:32 -0400

    added toNumber() toCurrency() toDate() toZero() to global service

--- client/src/app/services/global.service.ts
+++ client/src/app/services/global.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +7,37 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
   snackBarText = '';
   
-  constructor() { }
+  constructor(
+    private currencyPipe: CurrencyPipe
+  ) { }
+
+  toZero(val: any) {
+    return val ? val : 0;
+  }
+
+  toNumber(val: string) {
+    if (val) {
+      // remove all none-digit characters; remove all leading zeroes
+      return parseInt(val.replace(/\D/g, '').replace(/^0*/, ''), 10);
+    } else {
+      return 0;
+    }
+  }
+
+  toCurrency(val: any) {
+    if (val) {
+      return this.currencyPipe.transform(val.toString().replace(/\D/g, '').replace(/^0{2,}/, '0'), 'USD', 'symbol', '1.0-0');
+    } else {
+      return '$0';
+    }
+  }
+
+  toDate(val: any) {
+    if (val) {
+      return new Date(val);
+    } else {
+      return null;
+    }
+  }
+
 }
