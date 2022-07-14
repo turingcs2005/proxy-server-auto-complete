f104d2c9f281d1cffe1924a264e1d6a0edcd96f6
Rui Wang <ruwang@hanover.com>
2022-07-14 15:32:06 -0400

    updated global service

--- client/src/app/app-data/app-models.ts
+++ client/src/app/app-data/app-models.ts
@@ -2,3 +2,37 @@ export interface TreeNode {
   name: string;
   children?: TreeNode[];
 }
+
+
+// return display type base on field name
+export class DisplayType {
+  
+  static currencyFields = ['TIV', 'additionalExposure', 'directPropertyPremium', 'retentionLimit', 'limitPurchased', 
+  'reinsurancePremium', 'commissionAmount', 'policyTIV', 'grossReinsurancePremium', 'reinsuranceCommission', 
+  'HanoverNetRetention', 'facultativeAmountPurchased', 'totalInsuredValue', 'grossPremium', 'cededRetention', 'cededLimit',
+  'commission', 'netReinsurancePremium', 'retention', 'layer', 'retentionAmount'];
+
+  static dateFields = ['policyEffectiveDate', 'policyExpirationDate', 'midTermFacAdjustmentDate', 'createdAt', 
+  'updatedAt', 'deletedAt', 'dateOfPreparation', 'dateOfEndorsement', 'dateOfCancellation', 'cancellationDate', 'endorsementDate',
+  'facExpirationDate', 'facEffectiveDate', 'reinsuranceEffectiveDate', 'reinsuranceExpirationDate'];
+
+  constructor() {}
+
+  static currencyCheck(x: string) {
+    return DisplayType.currencyFields.includes(x);
+  }
+
+  static dateCheck(x: string) {
+    return DisplayType.dateFields.includes(x);
+  }
+
+  static displayType(x: string) {
+    if (DisplayType.currencyCheck(x)) {
+      return 'currency';
+    } else if (DisplayType.dateCheck(x)) {
+      return 'date';
+    } else {
+      return 'other';
+    }
+  }
+}
--- client/src/app/services/global.service.ts
+++ client/src/app/services/global.service.ts
@@ -1,10 +1,63 @@
 import { Injectable } from '@angular/core';
+import { DisplayType } from '../app-data/app-models';
+import { CurrencyPipe } from '@angular/common';
+import { UntypedFormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
   snackBarText = '';
+  currencyCheck = DisplayType.currencyCheck;
+  dateCheck = DisplayType.dateCheck;
   
-  constructor() { }
+  constructor(
+    private currencyPipe: CurrencyPipe
+  ) { }
+
+  // convert zero, null, undefined, '', false to zero
+  toZero(val: any) {
+    return val ? val : 0;
+  }
+
+  toNumber(val: string) {
+    if (val) {
+      // remove all none-digit characters; remove all leading zeroes
+      return parseInt(val.replace(/\D/g, '').replace(/^0*/, ''), 10);
+    } else {
+      return 0;
+    }
+  }
+
+  toCurrency(val: any) {
+    if (val) {
+      return this.currencyPipe.transform(val.toString().replace(/\D/g, '').replace(/^0{2,}/, '0'), 'USD', 'symbol', '1.0-0');
+    } else {
+      return '$0';
+    }
+  }
+
+  toDate(val: any) {
+    if (val) {
+      return new Date(val);
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * after loading a name-value pair from database, patch the value to a reactive form
+   */
+  patchValueToFormGroup (fg: UntypedFormGroup, name: string, value: any) {
+    if ((<UntypedFormGroup>fg).contains(name)) {
+      if (this.currencyCheck(name)) {    // if value is currency, convert from number to currency string before patching value
+        fg.get(name)!.setValue(this.toCurrency(value));  
+      } else if (this.dateCheck(name)) { // if value is date, convert from string to date before patching value
+        fg.get(name)!.setValue(this.toDate(value));
+      }else {                            // anything else, patch value straight
+        fg.get(name)!.setValue(value);
+      }
+    }
+  }
+
 }
